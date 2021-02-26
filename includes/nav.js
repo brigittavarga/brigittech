ca33a88eec4e8f1a77c985c5a8d5a031978406fa
Brigitta Varga <v.brigitta.zs@gmail.com>
2021-02-26 18:01:34 +0100

    adjusted mobile first

---
+++ includes/nav.js
@@ -0,0 +1,43 @@
+document.write(`
+<nav>
+
+<!-- svg logo icon -->
+<svg class="logo_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 432.78 444.91"><defs><style>.cls-1{fill:#0b0b0b;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" width="53.66" height="444.91" rx="26.83"/><circle class="cls-1" cx="213.01" cy="305.61" r="139.3"/><circle class="cls-1" cx="153.31" cy="80.23" r="80.23"/><circle class="cls-1" cx="387.78" cy="399.92" r="45"/></g></g></svg>
+
+<ul class="menu-links">
+   <a href="about.html"><li>About</li></a>
+   <a href="#portfolio"><li>Portfolio</li></a>
+   <a href="contact.html"><li>Contact</li></a>
+</ul>
+
+<!-- toggle icon OPEN -->
+<svg onclick="toggleFunction()" class="toggle-icon toggle-open" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="60" viewBox="0 0 15 60">
+   <defs>
+       <clipPath id="clip-toggle_icon">
+       <rect width="15" height="60"/>
+       </clipPath>
+   </defs>
+   <g id="toggle_icon" clip-path="url(#clip-toggle_icon)">
+       <g id="Group_1" data-name="Group 1" transform="translate(-1829 -52)">
+       <circle id="Ellipse_1" data-name="Ellipse 1" cx="7.5" cy="7.5" r="7.5" transform="translate(1829 52)" fill="#030303"/>
+       <circle id="Ellipse_2" data-name="Ellipse 2" cx="7.5" cy="7.5" r="7.5" transform="translate(1829 75)" fill="#030303"/>
+       <circle id="Ellipse_3" data-name="Ellipse 3" cx="7.5" cy="7.5" r="7.5" transform="translate(1829 97)" fill="#030303"/>
+       </g>
+   </g>
+</svg>
+
+<!-- toggle icon CLOSE X -->
+<svg onclick="toggleFunction()" class="toggle-close toggle-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="60" height="60" viewBox="0 0 60 60">
+   <defs>
+     <clipPath id="clip-toggle_iconX">
+       <rect width="60" height="60"/>
+     </clipPath>
+   </defs>
+   <g id="toggle_iconX" clip-path="url(#clip-toggle_iconX)">
+     <path id="Path_30" data-name="Path 30" d="M106,4513.114l47.131-49.837" transform="translate(-99.5 -4457.777)" fill="none" stroke="#0d0d0d" stroke-linecap="round" stroke-width="8"/>
+     <path id="Path_31" data-name="Path 31" d="M153.131,4513.114,106,4463.277" transform="translate(-99.5 -4457.777)" fill="none" stroke="#0d0d0d" stroke-linecap="round" stroke-width="8"/>
+   </g>
+ </svg>
+
+</nav>
+`)
---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuLinks = document.querySelector(".menu-links");
+const toggleClose = document.querySelector(".toggle-close");
+const toggleOpen = document.querySelector(".toggle-open");
+
+function toggleFunction() {
+    if (menuLinks.style.opacity === "1") {
+      menuLinks.style.opacity = "0";
+      toggleOpen.style.opacity = "1";
+      toggleClose.style.opacity = "0";
+    } else {
+      menuLinks.style.opacity = "1";
+      toggleOpen.style.opacity = "0";
+      toggleClose.style.opacity = "1";
+    }
+  }
