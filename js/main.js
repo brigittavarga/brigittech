d4cc05e377dd4743e31133047821d99529ea1904
Brigitta Varga <v.brigitta.zs@gmail.com>
2021-02-28 20:30:06 +0100

    added cursor interactivity

--- js/main.js
+++ js/main.js
@@ -2,6 +2,43 @@ const menuLinks = document.querySelector(".menu-links");
 const toggleClose = document.querySelector(".toggle-close");
 const toggleOpen = document.querySelector(".toggle-open");
 
+const mouseCursor = document.querySelector(".cursor");
+const navLinks = document.querySelectorAll(".menu-links a");
+const footer = document.querySelector("footer");
+const btnToSite = document.querySelectorAll(".btn-to-site");
+
+window.addEventListener("mousemove", cursor);
+
+// Mousemove function 
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
+
+// Make the cursor circle grow on mouseover
+navLinks.forEach(link => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+  });
+});
+
+/* Changing the color of the mouse on white or black background */
+function mouseWhite() {
+  mouseCursor.classList.add("mouse-white");
+}
+
+function mouseBlack() {
+  mouseCursor.classList.remove("mouse-white");
+}
+
+footer.addEventListener("mouseover", mouseWhite);
+footer.addEventListener("mouseleave", mouseBlack);
+btnToSite.addEventListener("mouseover", mouseWhite);
+btnToSite.addEventListener("mouseleave", mouseBlack);
+
 // Toggle menu function 
 function toggleFunction() {
     if (menuLinks.style.opacity === "1") {
@@ -14,4 +51,3 @@ function toggleFunction() {
       toggleClose.style.opacity = "1";
     }
   }
-
